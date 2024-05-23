d492be0fbbaabf15c769a63e2277ce4586ad83ad
Veronicol <veronica.celemin@gmail.com>
2024-05-23 08:41:24 +0200

    feat: add react query wrapper to app

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { appRoutes } from "./routes.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const router = createBrowserRouter(appRoutes);
 
+const queryClient = new QueryClient();
+// aquí podemos pasar opciones por defecto para decirle cómo queremos que se
+// comporte nuestra aplicación
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
