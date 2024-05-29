c492b576700ef39626369bf25db078d1258f0800
Veronicol <veronica.celemin@gmail.com>
2024-05-30 00:36:14 +0200

    feat: add new episode button

--- src/components/EpisodeList.tsx
+++ src/components/EpisodeList.tsx
@@ -4,14 +4,23 @@ import { useNavigate } from "react-router-dom";
 import { getIdxFromUrl } from "../utils/getIdxFromUrl";
 import { getEpisodeFromIdx } from "../utils/getEpisodeFromIdx";
 import { SwResponse } from "../lib/models/sw-response.model";
+import episodeVII from "./../utils/episodeVII.json";
 
 const fetchEpisodes = async (): Promise<SwResponse<Film>> => {
   const response = await fetch("https://swapi.dev/api/films");
   if (!response.ok) {
-    throw new Error('SW response was not OK');
+    throw new Error("SW response was not OK");
   }
   return response.json();
-}
+};
+
+const addEpisode = async () => {
+  return new Promise(function (resolve, _reject) {
+    setTimeout(() => {
+      resolve(episodeVII);
+    }, 500);
+  });
+};
 
 export const EpisodeList = ({ episodeIdx }: { episodeIdx?: string }) => {
   const navigate = useNavigate();
@@ -54,29 +63,42 @@ export const EpisodeList = ({ episodeIdx }: { episodeIdx?: string }) => {
           Ooops... something went wrong
         </div>
       ) : (
-        <div className="flex flex-col justify-start pl-4">
-          {episodeList.map((film) => (
-            <button
-              key={film.episode_id}
-              className={`border-b border-1 border-gray-400 w-full p-2 flex ${
-                itemSelected === film.episode_id
-                  ? "bg-slate-200"
-                  : "bg-white hover:bg-slate-100"
-              }`}
-              onClick={() => {
-                setItemSelected(film.episode_id);
-                const episodeIndex = getIdxFromUrl(film.url);
-                navigate(`/episode/${episodeIndex}`);
-              }}
-            >
-              <div className="font-semibold grow text-start">
-                {`0${film.episode_id} _ ${film.title}`}
-              </div>
-              <div className="italic w-30">
-                {film.director}, {film.release_date.split("-")[0]}
-              </div>
-            </button>
-          ))}
+        <div className="flex flex-col gap-6 items-end">
+          <div className="flex flex-col justify-start pl-4 w-full">
+            {episodeList.map((film) => (
+              <button
+                key={film.episode_id}
+                className={`border-b border-1 border-gray-400 w-full p-2 flex ${
+                  itemSelected === film.episode_id
+                    ? "bg-slate-200"
+                    : "bg-white hover:bg-slate-100"
+                }`}
+                onClick={() => {
+                  setItemSelected(film.episode_id);
+                  const episodeIndex = getIdxFromUrl(film.url);
+                  navigate(`/episode/${episodeIndex}`);
+                }}
+              >
+                <div className="font-semibold grow text-start">
+                  {`0${film.episode_id} _ ${film.title}`}
+                </div>
+                <div className="italic w-30">
+                  {film.director}, {film.release_date.split("-")[0]}
+                </div>
+              </button>
+            ))}
+          </div>
+          <button
+            className={`mr-4 border border-1 border-gray-400 py-1 px-3 rounded-md ${
+              episodeList.length === 7
+                ? "text-gray-400"
+                : "bg-slate-100 hover:bg-slate-200"
+            } `}
+            // onClick={() => postEpisode()}
+            disabled={episodeList.length === 7}
+          >
+            Add episode VII
+          </button>
         </div>
       )}
     </>
